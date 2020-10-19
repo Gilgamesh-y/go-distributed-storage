e9131ff98644ac924340e354791bf21ca76aecb1
Gilgamesh-y <805655528@qq.com>
2020-10-19 16:59:58 +0800

    add: multipart route

--- route/router.go
+++ route/router.go
@@ -10,11 +10,24 @@ func Load(g *gin.Engine) *gin.Engine {
 	g.Use(gin.Recovery())
 	g.Use(Options)
 
-	g.POST("upload", file.Upload)
 	g.GET("files", file.Get)
 
 	g.POST("add_user", user.AddUser)
 	g.GET("get_user/:id", user.GetUser)
+
+	f := g.Group("/file")
+	{
+		fs := f.Group("/single")
+		{
+			fs.POST("upload", file.Upload)
+		}
+		fm := f.Group("/multipart")
+		{
+			fm.POST("init", file.InitMultipartUploadInfo)
+			fm.POST("upload", file.MultipartUpload)
+			fm.POST("upload_complete", file.MultipartUploadComplete)
+		}
+	}
 
 	return g
 }
